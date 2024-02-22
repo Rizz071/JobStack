f7586f2211c3c7b29f65c6e50977a0975c481bca
Igor071 <iburykin071@gmail.com>
2024-02-23 01:52:03 +0200

    uibuild

--- JobStack-backend/types.ts
+++ JobStack-backend/types.ts
@@ -1,20 +1,20 @@
 export interface UserObject {
-	username: string;
-	fullname: string;
-	password: string;
+    username: string;
+    fullname: string;
+    password: string;
 }
 
 export interface JobObject {
-	job_title: string;
-	job_desc: string;
-	date_of_apply: Date;
-	current_status_desc: string;
-	active: boolean;
+    job_title: string;
+    job_desc: string;
+    date_of_apply: Date;
+    current_status_desc: string;
+    active: boolean;
 }
 
 export interface StatusObject {
-	position: number;
-	status: string;
-	status_desc: string;
-	date: Date;
+    position: number;
+    status: string;
+    status_desc: string;
+    date: Date;
 }
